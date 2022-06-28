556c24a8ed6b616dc6bc94ecfec989483d0c56a9
luckystriker16 <75381524+luckystriker16@users.noreply.github.com>
2022-06-28 22:49:54 +0200

    dvc.1.22061.0
    - Lucky_UI wurde auf Github verschoben, die aktuelle Version befindet sich nun dort.
    - version_history wurde aktualisiert:
      --> Es gibt nun einen Hintergrundeffekt, um das Fenster besser abzuheben.
    - updat_notification wurde hinzugefügt:
      -->Es gibt nun ein Fenster, welches anzeigt, wenn es ein Update gab.

---
+++ stylesheets/update_notification/updatelist.js
@@ -0,0 +1,13 @@
+var updatelist = [{
+    "id":[{
+        "name":"dvc 1.22061.0",
+        "description":"<ul><li>Lucky_UI wurde auf Github verschoben, die aktuelle Version befindet sich nun dort.</li><li>version_history wurde aktualisiert: <ul><li>Es gibt nun einen Hintergrundeffekt, um das Fenster besser abzuheben.</li></ul></li><li>updat_notification wurde hinzugefügt: <ul><li>Es gibt nun ein Fenster, welches anzeigt, wenn es ein Update gab.</li></ul></li></ul>"
+    }]
+}]
+
+var template_XASLKJSDFÖLK = [{
+    "id":[{
+        "name":"1.XXXX.X",
+        "description":"Hier steht die Beschreibung"
+    }]
+}]
