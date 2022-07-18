11aebe9e87389ecca108146e330e76576fa2b734
luckystriker16 <75381524+luckystriker16@users.noreply.github.com>
2022-07-18 23:15:58 +0200

    update_notification update
    - Die Dateistruktur des Listenelements wurde angepasst.
    - Es gibt nun eine funktion u_n_archive_show(), welche alle Daten des Listenelements anzeigen kann.

--- stylesheets/update_notification/update_notification.js
+++ stylesheets/update_notification/update_notification.js
@@ -18,20 +18,20 @@ function load_update_notification_stylesheet(){
 }
 
 function u_n_close(){
-    localStorage.setItem("updatelist_stand", Object.keys(updatelist[0]).length);
+    localStorage.setItem("updatelist_stand", /*Object.keys(updatelist[0]).length*/updatelist2.updates.length);
     update_notification.classList = "u_n_closed";
 }
 
 function u_n_show(){
-    update_selected = updatelist[0][Object.keys(updatelist[0])[0]][0];
-    update_notification_title.innerHTML = update_selected.name;
+    update_selected = /*updatelist[0][Object.keys(updatelist[0])[0]][0]*/updatelist2.updates[0];
+    update_notification_title.innerHTML = update_selected.id;
     update_notification_content.innerHTML = update_selected.description;
     update_notification.classList = "u_n_opened";
 }
 
 function u_n_start(){
     if(localStorage.getItem("updatelist_stand")){
-        if(JSON.stringify(Object.keys(updatelist[0]).length) == localStorage.getItem("updatelist_stand")){
+        if(/*JSON.stringify(Object.keys(updatelist[0]).length)*/updatelist2.updates.length == localStorage.getItem("updatelist_stand")){
             return;
         }else{
             u_n_show();
@@ -48,4 +48,34 @@ function u_n_link(evt){
         flyin = true;
         version_history_open();
     }
+}
+
+function u_n_testfor_name(elem_id){
+    if(updatelist2.updates[elem_id].name != undefined){
+        return updatelist2.updates[elem_id].name;
+        console.log("u_n_name");
+    }else{
+        return "";
+    }
+}
+
+function u_n_testfor_date(elem_id){
+    if(updatelist2.updates[elem_id].date != undefined){
+        return updatelist2.updates[elem_id].date;
+        console.log("u_n_date");
+    }else{
+        return "";
+    }
+}
+
+function u_n_archive_show(){
+    var update_content = "";
+    for(i=0; i<updatelist2.updates.length;i++){
+        var date_tmp = u_n_testfor_date(i);
+        var name_tmp = u_n_testfor_name(i);
+        update_content = update_content +"<div><h3 style='display:flex; gap:0.5em; flex-wrap: wrap; align-items: center;'>"+ updatelist2.updates[i].id +"<span style='font-weight:normal;font-size:0.8em'>"+ name_tmp +"</span><span style='font-weight:normal;font-size:0.8em'>"+ date_tmp +"</span></h3>"+ updatelist2.updates[i].description +"</div>";
+    }
+    flyin_open(
+        "<h2>Updatearchiv</h2>"+ update_content
+    );
 }
--- stylesheets/update_notification/updatelist.js
+++ stylesheets/update_notification/updatelist.js
@@ -16,19 +16,27 @@ var updatelist = [{
         "description":"<ul><li>Lucky_UI wurde auf Github verschoben, die aktuelle Version befindet sich nun dort.</li><li>version_history wurde aktualisiert: <ul><li>Es gibt nun einen Hintergrundeffekt, um das Fenster besser abzuheben.</li></ul></li><li>updat_notification wurde hinzugefügt: <ul><li>Es gibt nun ein Fenster, welches anzeigt, wenn es ein Update gab.</li></ul></li></ul>"
     }]
     
-}]
+}];
 
-/*var template_XASLKJSDFÖLK = [{
-    "id":[{
-        "name":"1.XXXX.X",
-        "description":"Hier steht die Beschreibung"
-    },{
-    "id":[{
-        "name":"dvc.1.22071.0",
-        "description":"<ul><li>Lucky_UI Core wurde eingeführt. <ul><li>Zur Nutzung von Lucky_UI muss nun nur noch das lucky_ui_core.js script in die Website integriert werden. </li><li>lucky_ui_core enthält u.a. die Möglichkeit bestimmte Lucky_UI Module zu deaktivieren.</li><li>Alle benötigten Dateien werden automatisch integriert.</li><li>Da die Seite sich durch lucky_ui_core langsamer aufbaut, gibt es einen kurzen Ladebildschirm.</li></ul></li></ul>"
-    },
-    {
-        "name":"dvc 1.22063.0",
-        "description":"<ul><li>Erster nutzbarer Ansatz für ein Scrollrespnives Design</li><li>Neuer Abschnitt: Tools <ul><li>Unter Tools werden sich Funktionen befinden, mit denen man realtiv einfach verschiedene Daten aus dem Browser abfragen kann. Z.B Geolocation Daten.</li></ul></li></ul>"
-    }]
-}]*/
+var updatelist2 = {
+    "updates": [
+        {
+            "id":"dvc.1.22071.0",
+            "name":"",
+            "date":"",
+            "description":"<ul><li>Lucky_UI Core wurde eingeführt. <ul><li>Zur Nutzung von Lucky_UI muss nun nur noch das lucky_ui_core.js script in die Website integriert werden. </li><li>lucky_ui_core enthält u.a. die Möglichkeit bestimmte Lucky_UI Module zu deaktivieren.</li><li>Alle benötigten Dateien werden automatisch integriert.</li><li>Da die Seite sich durch lucky_ui_core langsamer aufbaut, gibt es einen kurzen Ladebildschirm.</li></ul></li></ul>"
+        },
+        {
+            "id":"dvc 1.22063.0",
+            "description":"<ul><li>Erster nutzbarer Ansatz für ein Scrollrespnives Design</li><li>Neuer Abschnitt: Tools <ul><li>Unter Tools werden sich Funktionen befinden, mit denen man realtiv einfach verschiedene Daten aus dem Browser abfragen kann. Z.B Geolocation Daten.</li></ul></li></ul>"
+        },
+        {
+            "id":"dvc 1.22062.0",
+            "description":"<ul><li>Die Startseite wurde mit Erläuterungen zu Lucky_UI ergänzt.</li><li>Designanpassungen: <ul><li>Errorbenachrichtigung: <ul><li>Kollision mit watermark in der mobilen Ansicht behoben.</li><li>Fehler behoben durch den die Benachrichtigung mitgescrollt ist.</li></ul></li></ul></li><li>Neuer Abschnitt: Scroll <ul><li>Bei Scroll handelt es sich um eine sehr experimentelle Entwicklung eines Scrollresponsive Designs.</li></ul></li></ul>"
+        },
+        {
+            "id":"dv 1.22061.0",
+            "description":"<ul><li>Lucky_UI wurde auf Github verschoben, die aktuelle Version befindet sich nun dort.</li><li>version_history wurde aktualisiert: <ul><li>Es gibt nun einen Hintergrundeffekt, um das Fenster besser abzuheben.</li></ul></li><li>updat_notification wurde hinzugefügt: <ul><li>Es gibt nun ein Fenster, welches anzeigt, wenn es ein Update gab.</li></ul></li></ul>"
+        }
+    ]
+}
